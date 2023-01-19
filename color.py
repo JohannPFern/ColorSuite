7342020f4da50ab308c1bc71455a7f5e8c3c3b3c
JohannSCU <jfernando@scu.edu>
2023-01-18 19:53:11 -0800

    basic color class given rbg generate hls given color gets complementary

--- color.py
+++ color.py
@@ -1,28 +1,30 @@
 import colorsys
+import random
 
 
 class Color:
     """
     Create by passing in r,g,b values in [0,255]
-    rbg values preserved (rounded) in [0,255] This might change eventually if more math is done in [0,1] space?
+    RGB values (int rounded) in [0,255] This might change eventually if more math is done in [0,1] space?
     Rounding to prevent float .000001 and .99999 error propagation, and ensure consistency
 
-    hls values generated and saved as floats in [0,1]
+    HLS values generated and saved as floats in [0,1]
     """
 
     def __init__(self, r, g, b):
         """
         :param r,g,b: int in [0,255]
         """
-        self.red = round(r)
-        self.green = round(g)
-        self.blue = round(b)
-        self.hue, self.luminosity, self.saturation = colorsys.rgb_to_hls(r, g, b)
+        self._red = round(r)
+        self._green = round(g)
+        self._blue = round(b)
+        self._hue, self._luminosity, self._saturation = colorsys.rgb_to_hls(r, g, b)
 
     @staticmethod
     def hex_to_rgb(hex_code):
         """
         helper function to prep input to constructor
+
         :param hex_code: ex: "123456"
         :return: ex: 18, 52, 86
         """
@@ -31,25 +33,45 @@ class Color:
         b = int(hex_code[4:], 16)
         return r, g, b
 
-    # Pass n where n is the total number of colors in the scheme
     def get_reg_scheme(self, n):
         """
         Produce a scheme of n colors, including the base, which are evenly distributed around the hue wheel
         The points form a regular shape (or line) hence the name
 
         Saturation and Luminosity are IRRELEVANT to this process
+
         :param n: number of colors in scheme
         :return: list of colors
         """
         colors = []
-        new_hue = self.hue
+        new_hue = self._hue
         for i in range(n - 1):
             new_hue += 1 / n
             if new_hue > 1:
                 new_hue -= 1
-            r, g, b = colorsys.hls_to_rgb(new_hue, self.luminosity, self.saturation)
+            r, g, b = colorsys.hls_to_rgb(new_hue, self._luminosity, self._saturation)
             colors.append(Color(r, g, b))
         return colors
+
+    def get_complement(self):
+        """
+        A degenerate case of regular scheme, where only 1 color is returned
+        A separate method is called for ease of development as complements will (probably) be frequently used
+
+        :return: the color with equal S & L, and opposite H
+        """
+        return self.get_reg_scheme(2)[0]
+
+    def get_accent(self):
+        """
+        Obviously the implementation is not final
+        This placeholder shows how generators will be implemented to produce colors in particular patterns
+
+        :yield: a new Color which might be a decent accent
+        """
+        comp = self.get_complement()
+        while 1:
+            yield comp
 
     def __str__(self):
         """Uncomment desired print option"""
@@ -57,7 +79,7 @@ class Color:
         # return f"{self.hue}{self.luminosity}{self.saturation}"
 
         # Print HEX
-        return f"{hex(self.red)[2:]}{hex(self.green)[2:]}{hex(self.blue)[2:]}"
+        return f"{hex(self._red)[2:]}{hex(self._green)[2:]}{hex(self._blue)[2:]}"
 
         # Print RBG
         # return f"{r}|{g}|{b}"
@@ -67,10 +89,19 @@ class Color:
         return str(self)
 
 
-# Test Block
-in_color = "123456"
-in_red, in_green, in_blue = Color.hex_to_rgb(in_color)
+if __name__ == "__main__":
+    # Test Block
+    in_color = "123456"
+    in_red, in_green, in_blue = Color.hex_to_rgb(in_color)
 
-base = Color(in_red, in_green, in_blue)
-scheme = base.get_reg_scheme(4)
-print(*scheme, sep="\n")
+    base = Color(in_red, in_green, in_blue)
+    accents = base.get_accent()
+
+    in_color2 = "654321"
+    in_red2, in_green2, in_blue2 = Color.hex_to_rgb(in_color2)
+
+    base2 = Color(in_red2, in_green2, in_blue2)
+    accents2 = base2.get_accent()
+
+    print(next(accents))
+    print(next(accents2))
